c631085f43a9db95f6cd7118dce66724edd83833
Cherry Xu <cherryxuqianyun@gmail.com>
2018-03-11 21:57:13 -0700

    Sum of Two Integers - positives integers only using 8 bit

---
+++ leetcode/sumOfTwoIntegers.js
@@ -0,0 +1,40 @@
+// Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+// Example:
+// Given a = 1 and b = 2, return 3.
+
+// https://leetcode.com/problems/sum-of-two-integers/description/
+
+const getSum = (a, b) => {
+  let bitA;
+  let bitB;
+  let temp = new Array(8);
+  temp.fill(0);
+
+
+  Math.sign(a) === 1 ? bitA = a.toString(2) : bitA = (a * -1).toString(2);
+  Math.sign(b) === 1 ? bitB = b.toString(2) : bitB = (b * -1).toString(2);
+  bitA = ('00000000' + bitA).substr(-8);
+  bitB = ('00000000' + bitB).substr(-8);
+
+  if (Math.sign(a) === Math.sign(b)) {
+    for (let i = 7; i >= 0; i--) {
+      if (bitA[i] === '1' && bitB[i] === '1') {
+        temp[i - 1] = 1;
+        console.log(temp);
+      } else if ((bitA[i] === '1' && bitB[i] === '0') || (bitA[i] === '0' && bitB[i] === '1')) {
+        if (temp[i] === 1) {
+          temp[i - 1] = 1;
+          temp[i] = 0;
+        } else {
+          temp[i] = 1;
+        }
+      }
+    }
+  }
+
+  temp = parseInt(temp.join(''), 2);
+  return temp;
+}
+
+console.log(getSum(15, 120));
