b0b39f8c80dbd59ac10b586a4caaf7ef012fe2c6
Cherry Xu <cherryxuqianyun@gmail.com>
2018-03-13 19:27:01 -0700

    largest sum sub-array - dynamic

--- dynamic-programing/largestSumSubarray.js
+++ dynamic-programing/largestSumSubarray.js
@@ -1,22 +1,48 @@
+// const largestSumSubarray = (arr) => {
+//   let globalMax = arr[0];
+//   let currentMax = arr[0];
+  
+//   for (let i = 1; i < arr.length; i++) {
+//     let num = arr[i];
+    
+//     if (currentMax + num > num) {
+//       currentMax = currentMax + num;
+//     } else {
+//       currentMax = num;
+//     }
+
+//     if (currentMax > globalMax) {
+//       globalMax = currentMax;
+//     } 
+//   }
+
+//   return globalMax;
+// }
+
 const largestSumSubarray = (arr) => {
   let globalMax = arr[0];
-  let currentMax = arr[0];
-  
-  for (let i = 1; i < arr.length; i++) {
-    let num = arr[i];
-    
-    if (currentMax + num > num) {
-      currentMax = currentMax + num;
-    } else {
-      currentMax = num;
-    }
+  let globalStart= 0;
+  let globalEnd = 0;
+  let localStart = 0;
+  let localEnd = 0;
+  let maxArray = new Array(arr.length);
+  maxArray[0] = arr[0];
 
-    if (currentMax > globalMax) {
-      globalMax = currentMax;
-    } 
+  for (let i = 1; i < arr.length; i++) {
+    let localMax = Math.max(arr[i], (arr[i] + maxArray[i - 1]));
+    maxArray[i] = localMax;
+
+    if (localMax === arr[i]) localStart = i;
+    localEnd = i;
+
+    if (localMax > globalMax) {
+      globalMax = localMax;
+      globalStart = localStart;
+      globalEnd = localEnd; 
+    }
   }
 
-  return globalMax;
-}
+  return [globalStart, globalEnd];
+};
 
-console.log(largestSumSubarray([1, 2, -4, -3, 5, 7]));
+console.log(largestSumSubarray([-4, 2, -5, 1, 2, 3, 6, -5, 1]));
